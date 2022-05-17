25f0c7b158f9301ba548fce079064defe222bdd7
Grogros4 <35577365+Grogros4@users.noreply.github.com>
2022-05-17 09:39:03 +0200

    light event

--- src/event.cpp
+++ src/event.cpp
@@ -6,10 +6,52 @@
 
 using namespace cgp;
 
-bool events::is_visible(float x, float y, float z) {
-	return((x - pos.x) * (x - pos.x) + (y - pos.y * (y - pos.y) + (z - pos.z) * (z - pos.z) > rayon * rayon);
+
+
+void events::push_event(event a) {
+	event_queue.push(a);
 }
 
-void events::update_rayon(float time, float c) {
-	rayon = rayon + c * time;
+
+
+/*
+bool events::is_visible(float x, float y, float z, float c) {
+	float rayon = c * global_time;
+	return((x - pos.x) * (x - pos.x) + (y - pos.y * (y - pos.y) + (z - pos.z) * (z - pos.z) > rayon * rayon));
 }
+
+void events::update_rayon(float time) {
+	global_time = global_time + time;
+}
+
+
+void lights::initialize_light(float x, float y, float z, std::string light_name, float f) {
+	pos = { x,y,z };
+	frequency = f;
+	name = light_name;
+	clock = 0;
+	status = false;
+	mesh sphere_mesh = mesh_primitive_sphere();
+	light_source.initialize(sphere_mesh, light_name);
+	light_source.transform.translation = pos;
+	light_source.shading.color = { 1.0f, 1.0f, 1.0f };
+}
+
+void lights::update_clock(float time) {
+	clock = clock + time;
+	if (clock >= frequency) {
+		clock = 0;
+		status =  !status;
+		event_queue.pop();
+	}
+}
+
+void lights::update_color() {
+	if (status) {
+		light_source.shading.color = { 0.0f, 0.0f, 0.0f };
+	}
+	else {
+		light_source.shading.color = { 1.0f, 1.0f, 1.0f };
+	}
+}	
+*/
--- src/event.hpp
+++ src/event.hpp
@@ -1,12 +1,37 @@
 #pragma once
-
+#include <queue>
 #include "cgp/cgp.hpp"
+struct event {
+	float creation_date;
+	int id;
+};
+
+
+
 
 struct events {
-	float rayon;
+	float global_time;
 	cgp::vec3 pos;
+	std::queue<event> event_queue;
 
 	//Functions
-	bool is_visible(float x, float y, float z);
-	void update_rayon(float time, float c);
+	void push_event(event a);
+	void activate();
+};
+
+
+struct lights {
+
+	std::string name;
+	float frequency;
+	private : float clock;
+	bool status; //True = on, False = off
+	cgp::mesh_drawable light_source; 
+	cgp::vec3 pos;
+	events event_queue;
+
+	void initialize_light(float x, float y, float z, std::string name, float f);
+	void update_clock(float time);
+	void update_color();
+	//cgp::mesh create_light(int r);
 };
